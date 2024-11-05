595ad29434db9d609bbcb0345ebf7e01ccab3fa1
I hate iu <tranhung10122003@gmail.com>
2024-11-05 15:23:13 +0700

    feat: initialize a project

---
+++ src/test/java/com/us/gamewithserver/GameWithServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.us.gamewithserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameWithServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
